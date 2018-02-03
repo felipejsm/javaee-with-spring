3771b9a472a105a8b665632e96b007e8116b92d7
felipejsm <felipejsm41@gmail.com>
2018-02-03 17:44:12 -0200

    JDBC config

---
+++ workspace/app/src/main/resources/DatabaseConnectionFactory.java
@@ -0,0 +1,54 @@
+/**
+ * Singleton Factory class to create JDBC database connections
+ *
+ */
+public class DatabaseConnectionFactory {
+	private static DatabaseConnectionFactory conFactory = new DatabaseConnectionFactory();
+	private DataSource dataSource = null;
+
+	private DatabaseConnectionFactory() {
+
+	}
+
+	/**
+	* Must be called before any other method in this class.
+	* Initializes the data source and saves it in an instance
+	variable
+	*
+	* @throws IOException
+	*/
+	public synchronized void init() throws IOException {
+	//Check if init was already called
+		if (dataSource != null)
+			return;
+			//load db.properties file first
+			InputStream inStream = this.getClass().getClassLoader().getResourceAsStream("db.properties");
+			Properties dbProperties = new Properties();
+			dbProperties.load(inStream);
+			inStream.close();
+			//create Tomcat specific pool properties
+			PoolProperties p = new PoolProperties();
+			p.setUrl("jdbc:mysql://" + dbProperties.getProperty("db_host") +
+			":" + dbProperties.getProperty("db_port") + "/" +
+			dbProperties.getProperty("db_name"));
+			p.setDriverClassName(dbProperties.getProperty("db_driver_class_
+			name"));
+			p.setUsername(dbProperties.getProperty("db_user_name"));
+			p.setPassword(dbProperties.getProperty("db_password"));
+			p.setMaxActive(10);
+			dataSource = new DataSource();
+			dataSource.setPoolProperties(p);
+	}
+
+	// Provides access to singleton instance
+	public static DatabaseConnectionFactory getConnectionFactory() {
+		return conFactory;
+	}
+
+	// returns database connection object
+	public Connection getConnection() throws SQLException {
+		if (dataSource == null)
+			throw new SQLException("Error initializing datasource");
+		return dataSource.getConnection();
+	}
+}}
